ae0e896c010dbb981975f2b3d64a188ef773271d
Shreyansh Chauhan <shreyanshch07@gmail.com>
2026-05-01 14:07:48 +0530

    Connect frontend to live backend

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "splendid-imagination-production-3a81.up.railway.app"
+  baseURL: "https://splendid-imagination-production-3a81.up.railway.app/api"
 });
 
 export default instance;
